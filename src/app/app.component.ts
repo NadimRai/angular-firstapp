b63dc59c5cc7cbc33a513d81855e47ab764fc0b1
NadimRai <rai_nadim@yahoo.com>
2017-02-21 16:19:55 -0800

    connecting app and other component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
+import { OtherComponent} from './other/other.component';
 
 @Component({
   selector: 'app-root',
-  template: `
-  	<h1>Inline Template</h1>
-  	<p>Another Line</p>
+  template:`
+  	<h1>Inline Templates</h1>
+  	<app-other></app-other>
   	`,
   styles: [`
   	h1{
   	color: red;
-  	`]
+  	`],
+  	
 })
 export class AppComponent {
   
